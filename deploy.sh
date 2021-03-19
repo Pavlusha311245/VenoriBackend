c8a2fc696e3f7c8d1619e045e98d206497df572b
macerares <macerares@mail.ru>
2021-03-19 16:55:25 +0300

    last check

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 echo "Deploy script started"
 cd /var/www/html
 git pull
-composer install --no-plugins --no-scripts
+echo "=====PULLED====="
+composer install --no-scripts
+echo "=====INSTALLED====="
+php artisan migrate
+echo "=====MIGRATED====="
+php artisan optimize
+echo "=====OPTIMIZED====="
 echo "Deploy script finished execution"
