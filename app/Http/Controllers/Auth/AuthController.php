c930784478132b5cc8e5757ff96cca7c102e2427
Paul Zavadski <zavadskiy.pavel2002@outlook.com>
2021-03-22 14:31:05 +0300

    Registration made

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Validator;
+
+class AuthController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function postRegistration(Request $request): JsonResponse
+    {
+        $validData = $request->validate([
+            'first_name' => 'required|min:2',
+            'second_name' => 'required|min:2',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8'
+        ]);
+        $validData['password'] = bcrypt($validData['password']);
+
+        $user = User::create($validData);
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return response()->json(['message' => 'You were successfully registered. Use your email and password to sign in.', 'user' => $user, 'access_token' => $accessToken], 201);
+    }
+}
