7dd8de37a0365476bfe9906b9b84a477f82846ec
Evgeny Kondratovich <wiehr@mail.ru>
2021-03-22 12:54:47 +0000

    execute.sh edited online with Bitbucketasd

--- execute.sh
+++
@@ -1,8 +0,0 @@
-#/var/www/html
-php artisan down
-git pull origin develop
-composer install --no-dev --prefer-dist
-php artisan optimize
-php artisan migrate
-php artisan up
-echo 'Deploy finished.'
