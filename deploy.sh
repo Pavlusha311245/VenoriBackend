78f60907b075d5e81562e2f8061419957f704a79
Evgeny Kondratovich <wiehr@mail.ru>
2021-03-22 13:42:27 +0000

    deploy.sh edited online with Bitbucket

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 echo "Deploy script started"
 cd /var/www/fullplate
 
+echo "=====PULLING====="
 git pull origin develop
 echo "=====PULLED====="
+
+echo "=====INSTALLING====="
 composer install --no-scripts
 echo "=====INSTALLED====="
-php artisan optimize
-echo "=====OPTIMIZED====="
+
+echo "=====MIGRATING====="
 php artisan migrate
 echo "=====MIGRATED====="
+
 echo "Deploy script finished execution"
