a4e2bc1304bf774b93711ecd14bc066344da6a4a
Evgeny Kondratovich <wiehr@mail.ru>
2021-03-22 13:35:38 +0000

    deploy.sh edited online with Bitbucket

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,8 @@ git pull origin develop
 echo "=====PULLED====="
 composer install --no-scripts
 echo "=====INSTALLED====="
-php artisan migrate
-echo "=====MIGRATED====="
 php artisan optimize
 echo "=====OPTIMIZED====="
+php artisan migrate
+echo "=====MIGRATED====="
 echo "Deploy script finished execution"
-
