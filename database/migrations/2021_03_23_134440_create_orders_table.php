0497815299f4ecc53453b981beec51f099c3da6b
Paul Zavadski <zavadskiy.pavel2002@outlook.com>
2021-03-23 17:26:25 +0300

    Add migrations

---
+++ database/migrations/2021_03_23_134440_create_orders_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->decimal('price');
+            $table->dateTime('datetime');
+            $table->string('people');
+            $table->string('special');
+            $table->string('staying');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('place_id');
+            $table->foreign('place_id')->references('id')->on('places');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
