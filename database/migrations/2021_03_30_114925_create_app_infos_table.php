d571bf94203511bf37bae7b58565662596eec566
Dzianis <kotickpro@gmail.com>
2021-03-30 15:21:24 +0300

    CRI-30 About, Contact, Terms, Privacy Policy
    Make: app_infos migration and added AppInfo model

---
+++ database/migrations/2021_03_30_114925_create_app_infos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('app_infos', function (Blueprint $table) {
+            $table->id();
+            $table->text('about');
+            $table->text('contact');
+            $table->text('terms');
+            $table->text('privacy_policy');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('app_info');
+    }
+}
