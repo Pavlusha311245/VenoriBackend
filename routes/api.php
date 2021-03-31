e0aa5058824a89286497897d720c9ea3e3b2337f
Dzianis <kotickpro@gmail.com>
2021-03-31 11:56:10 +0300

    CRI-12 CRUD for Products
    Added Route::resource('products', 'App\Http\Controllers\ProductController') at api

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::resource('categories', 'App\Http\Controllers\CategoryController');
+Route::resource('products', 'App\Http\Controllers\ProductController');
 Route::post('/login','App\Http\Controllers\Auth\AuthController@postLogin')->middleware('request.logging');
 Route::post('/registration','App\Http\Controllers\Auth\AuthController@postRegistration')->middleware('request.logging');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
