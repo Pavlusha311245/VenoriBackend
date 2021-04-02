a3c80ddb8a90c0b912addc78ba7c1241a3de8ab8
Paul <zavadksiy.pavel2002@outlook.com>
2021-04-02 14:30:28 +0300

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -22,17 +22,15 @@ Route::group(['middleware' => ['auth:api']], function() {
 //   Route::resource('roles', RoleController::class);
 //   Route::resource('users', UserController::class);
     Route::resource('categories', CategoryController::class);
+    Route::resource('products',ProductController::class);
     Route::post('/logout', 'App\Http\Controllers\Auth\AuthController@postLogout');
     Route::get('/details', 'App\Http\Controllers\UserController@showProfile');
-    Route::resource('places', PlaceController::class);
     Route::get('/booking_history', 'App\Http\Controllers\OrderController@getBookingHistory');
 //    Route::get('/users/{id}/booking_history', 'App\Http\Controllers\Auth\AuthController@getBookingHistoryById');
     Route::get('/users/{id}/location', 'App\Http\Controllers\UserController@getUserLocation');
     Route::resource('places', PlaceController::class);
-
+    Route::get('/places','App\Http\Controllers\PlaceController@searchPlace');
 });
-
-Route::get('/places','App\Http\Controllers\PlaceController@searchPlace');
 
 Route::post('/login','App\Http\Controllers\Auth\AuthController@postLogin')->middleware('logging.request');
 Route::post('/registration','App\Http\Controllers\Auth\AuthController@postRegistration')->middleware('logging.request');
