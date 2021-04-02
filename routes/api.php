88f274450ad59d8aeb247f437c4a5bf0e3862a44
Paul <zavadksiy.pavel2002@outlook.com>
2021-04-02 13:53:03 +0300

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -21,13 +21,13 @@ Route::group(['middleware' => ['auth:api']], function() {
 //   Route::resource('roles', RoleController::class);
 //   Route::resource('users', UserController::class);
     Route::resource('categories', CategoryController::class);
-
     Route::post('/logout', 'App\Http\Controllers\Auth\AuthController@postLogout');
     Route::get('/details', 'App\Http\Controllers\UserController@showProfile');
     Route::resource('places', PlaceController::class);
     Route::get('/booking_history', 'App\Http\Controllers\OrderController@getBookingHistory');
 //    Route::get('/users/{id}/booking_history', 'App\Http\Controllers\Auth\AuthController@getBookingHistoryById');
     Route::resource('places', PlaceController::class);
+    Route::get('/users/{id}/location', 'App\Http\Controllers\UserController@getUserLocation');
     Route::get('/places','App\Http\Controllers\PlaceController@searchPlace');
 });
 
