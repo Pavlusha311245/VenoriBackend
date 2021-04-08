34ded0254682fa2ab1a0c5c05533367f0c96e2b0
web-designer-life <io57196657@gmail.com>
2021-04-08 14:34:38 +0300

    add: files

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+/**
+ * CommentController for adding, deleting, updating and showing comments
+ *
+ * @package App\Http\Controllers
+ */
+class CommentController extends Controller
+{
+    /**
+     * The method returns a list of all comments
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return Comment::paginate(5);
+    }
+
+    /**
+     * The method adds a new comment
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|min:1',
+            'description' => 'required|min:1',
+            'review_id' => 'required',
+        ]);
+
+        $comment = Comment::create($request->all());
+
+        return response()->json($comment, 201);
+    }
+
+    /**
+     * The method returns comments.
+     *
+     * @param $id
+     * @return Response|string
+     */
+    public function show($id)
+    {
+        return Comment::findOrFail($id);
+    }
+
+    /**
+     * The method updates comment
+     *
+     * @param Request $request
+     * @param Comment $comment
+     * @return JsonResponse
+     */
+    public function update(Request $request, Comment $comment)
+    {
+        $request->validate([
+            'title' => 'string|min:1',
+            'description' => 'string|min:1',
+            'review_id' => 'numeric',
+        ]);
+
+        $comment->update($request->all());
+
+        return response()->json($comment, 200);
+    }
+
+    /**
+     * The method removes comment
+     *
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function destroy($id)
+    {
+        $comment = Comment::findOrFail($id);
+        $comment->delete();
+
+        return response()->json(['message' => 'Comment is deleted successfully'], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\FavouriteController;
 use App\Http\Controllers\PlaceController;
 use App\Http\Controllers\ProductController;
@@ -28,6 +29,7 @@ Route::group(['middleware' => ['auth:api']], function () {
     Route::resource('reviews', ReviewController::class);
     Route::resource('favourites', FavouriteController::class);
     Route::resource('schedules', ScheduleController::class);
+    Route::resource('comments', CommentController::class);
 
     Route::get('/details', 'App\Http\Controllers\UserController@showProfile');
     Route::get('/get_info', 'App\Http\Controllers\AppInfoController@getInfo');
