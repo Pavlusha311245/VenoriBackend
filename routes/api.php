2d414f8e4d769b33c5bb5ad502b981889695b224
Denis <kotickpro@gmail.com>
2021-04-12 17:54:55 +0300

    CRI-66 Upload Image of Categories / Users
    add ImageService and add initialize ImageService in UserController in upload function

--- routes/api.php
+++ routes/api.php
@@ -44,7 +44,6 @@ Route::group(['middleware' => ['auth:api']], function () {
     Route::post('/orders/{order_id}', 'App\Http\Controllers\OrderController@cancelOrder');
     Route::post('/logout', 'App\Http\Controllers\Auth\AuthController@logout');
 
-//    Route::post('upload', 'App\Http\Controllers\ImageController@upload_image');
 });
 
 Route::group(['middleware' => 'logging'], function () {
