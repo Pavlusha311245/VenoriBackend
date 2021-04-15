94f6f45e0e0bec5715414b0068114df3475572d7
Paul <zavadksiy.pavel2002@outlook.com>
2021-04-15 16:30:38 +0300

    add deploy_production.sh file

--- deploy_production.sh
+++ deploy_production.sh
@@ -1,9 +1,9 @@
 echo "Deploy script started"
-cd /var/www/fullplate
+cd /var/www/html
 
 echo "=====PULLING====="
-git reset --hard origin/develop
-git pull origin develop
+git reset --hard origin/master
+git pull origin master
 echo "=====PULLED====="
 
 echo "=====INSTALLING====="
