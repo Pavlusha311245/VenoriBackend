f64029c70ee46df1b72b0f39f87b363589cc5573
web-designer-life <io57196657@gmail.com>
2021-05-03 14:40:34 +0300

    changes

---
+++ resources/views/orders/index.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts.app')
+@section('title', 'Orders')
+@section('content')
+    <style>
+        table {
+            border-collapse: collapse;
+            border: 1px solid #9561e2;
+        }
+
+        select {
+            margin: 0;
+        }
+
+        th, td {
+            padding: 5px;
+        }
+
+        .table-row:not(:first-child) {
+            transition: all 0.5s;
+            padding: 1px;
+            background-color: rgba(255, 255, 255, 0.5);
+        }
+
+        .orders:not(:first-child):hover {
+            background-color: rgba(149, 97, 226, 0.5);
+        }
+
+        .update_form {
+            padding: 15px;
+            border-radius: 10px;
+            border: 1px solid #9561e2;
+            background-color: rgba(255, 255, 255, 0.5);
+        }
+    </style>
+
+    <div class="d-flex justify-content-center" style="height: 750px">
+        <div style="height: min-content">
+            @if(count($orders)==0)
+                <h2>There is no data to form the table</h2>
+                <p style="text-align: center"><a href="/admin/orders/create"><img src="https://img.icons8.com/nolan/64/plus.png" width="50" height="50"/></a></p>
+            @else
+                <table style="margin: 100px 0; min-width: 100%;">
+                    <tr style="background-color: rgba(122,117,226,0.5); text-align: center;">
+                        <th style="width: 50px">Id</th>
+                        <th>Status</th>
+                        <th>Type</th>
+                        <th>Rating</th>
+                        <th>Phone</th>
+                        <th>Capacity</th>
+                        <th>Table Price</th>
+                        <th>Description</th>
+                        <th>Address Full</th>
+                        <th>Address Lat</th>
+                        <th>Address Lon</th>
+                        <th>Image Url</th>
+                        <th>Details</th>
+                        <th>Remove</th>
+                    </tr>
+                    @foreach($orders as $order)
+                        <tr class="table-row">
+                            <td style="text-align: center" id="order_id">{{$order['id']}}</td>
+                            <td id="order_status">{{$order['status']}}</td>
+                            <td id="order_price">{{$order['price']}}</td>
+                            <td id="order_date">{{$order['date']}}</td>
+                            <td id="order_people">{{$order['people']}}</td>
+                            <td id="order_time">{{$order['staying']}}</td>
+                            <td id="order_price">{{$order['time']}}</td>
+                            <td id="order_staying_end">{{$order['staying_end']}}</td>
+                            <td id="order_user_id">{{$order['user_id']}}</td>
+                            <td id="order_place_id">{{$order['place_id']}}</td>
+                            <td>
+                                <a href="/admin/orders/{{$order->id}}" class="btn btn-outline-primary btn-sm">Show</a>
+                            </td>
+                            <td>
+                                <a href="/admin/orders/{{$place->id}}/delete" class="btn btn-danger btn-sm">Remove</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                    <tr class="table-row">
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td style="text-align: center"><a href="/admin/orders/create"><img src="https://img.icons8.com/nolan/64/plus.png" width="30" height="30"/></a></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    @endif
+                </table>
+        </div>
+    </div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Log;
+use App\Models\Order;
+use App\Models\Place;
+use App\Models\User;
+use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Spatie\Permission\Models\Role;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,24 +20,95 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    Log::channel('abuse') -> info('API endpoint abuse', [
-        'user_id' => 1
-    ]);
-});
-
-//Route::group(['middleware' => 'auth:api'], function() {
-Route::get('/dashboard', function () {
     return view('home');
 });
 
-Route::get('/products', function () {
-    return view('products');
+Route::group(['middleware' => ['auth:web', 'role:Admin'], 'prefix' => 'admin'], function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/products', function () {
+        return view('products.index', ['products' => Product::all()]);
+    });
+    Route::get('/products/create', function () {
+        return view('products.create', ['products' => Product::all()]);
+    });
+    Route::get('/products/{id}', function ($id) {
+        return view('products.show', ['products' => Product::findOrFail($id)]);
+    });
+    Route::get('/products/{id}/edit', function ($id) {
+        return view('products.edit', ['products' => Product::findOrFail($id)]);
+    });
+    Route::get('/products/{id}/delete', function ($id) {
+        return view('products.delete', ['products' => Product::findOrFail($id)]);
+    });
+
+    Route::get('/user/resetPassword', function () {
+        return view('auth.reset');
+    });
+
+    Route::get('/users', function () {
+        return view('users.index', ['users' => User::all()]);
+    });
+    Route::get('/users/create', function () {
+        return view('users.create', ['roles' => Role::all()]);
+    });
+    Route::get('/users/{id}', function ($id) {
+        return view('users.show', ['user' => User::findOrFail($id)]);
+    });
+    Route::get('/users/{id}/edit', function ($id) {
+        return view('users.edit', ['user' => User::findOrFail($id)]);
+    });
+    Route::get('/users/{id}/delete', function ($id) {
+        return view('users.delete', ['user' => User::findOrFail($id)]);
+    });
+
+    Route::get('/places', function () {
+        return view('places.index', ['places' => Place::all()]);
+    });
+    Route::get('/places/create', function () {
+        return view('places.create', ['places' => Place::all()]);
+    });
+    Route::get('/places/{id}', function ($id) {
+        return view('places.show', ['place' => Place::findOrFail($id)]);
+    });
+    Route::get('/places/{id}/edit', function ($id) {
+        return view('places.edit', ['place' => Place::findOrFail($id)]);
+    });
+    Route::get('/places/{id}/delete', function ($id) {
+        return view('places.delete', ['place' => Place::findOrFail($id)]);
+    });
+
+    Route::get('/orders', function () {
+        return view('orders.index', ['orders' => Order::all()]);
+    });
+
+    Route::post('/users/create', 'App\Http\Controllers\UserController@create');
+    Route::post('/users/{id}/edit', 'App\Http\Controllers\UserController@edit');
+    Route::post('/users/{id}/delete', 'App\Http\Controllers\UserController@remove');
+
+    Route::post('/places/create', 'App\Http\Controllers\PlaceController@create');
+    Route::post('/places/{id}/edit', 'App\Http\Controllers\PlaceController@edit');
+    Route::post('/places/{id}/delete', 'App\Http\Controllers\PlaceController@remove');
+
+    Route::post('/products/create', 'App\Http\Controllers\ProductController@create');
+    Route::post('/products/{id}/edit', 'App\Http\Controllers\ProductController@edit');
+    Route::post('/products/{id}/delete', 'App\Http\Controllers\ProductController@remove');
+
+    Route::post('/user/resetPassword', 'App\Http\Controllers\Auth\AuthController@resetPasswordView');
 });
 
-Route::get('/users', function () {
-    return view('users');
+Route::get('/login', function () {
+    if (Auth::check())
+        return view('home');
+    return view('auth.login');
+})->name('login');
+
+Route::post('/login', 'App\Http\Controllers\Auth\AuthController@loginAdmin');
+
+Route::get('/logout', function () {
+    Auth::logout();
+    return redirect('/login');
 });
 
-Route::get('/places', function () {
-    return view('places');
-});
